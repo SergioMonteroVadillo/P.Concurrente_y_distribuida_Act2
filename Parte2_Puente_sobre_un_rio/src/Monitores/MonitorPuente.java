eef2209a395bd95fedc0208d080a5f40f09e1156
SergioMonteroVadillo <84685043+SergioMonteroVadillo@users.noreply.github.com>
2023-02-11 13:33:00 +0100

    Add files via upload

---
+++ Parte2_Puente_sobre_un_rio/src/Monitores/MonitorPuente.java
@@ -0,0 +1,27 @@
+package Monitores;
+
+public class MonitorPuente {
+	
+	private boolean puenteLibre = true;
+	
+	public MonitorPuente () {		
+	}
+	
+	public synchronized void ocuparPuente () {
+		
+		while (!puenteLibre) {	//Bucle infinito que espera a que el puente este libre para ocuparlo
+			
+			try {
+				wait();		
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		puenteLibre = false;	//Pone el monitor como puente ocupado
+	}
+	
+	public synchronized void liberarPuente () {
+		puenteLibre = true;		//Pone el monitor como puente libre
+		notify();	//Informa a hilo a la espera que el puente está libre
+	}
+}
